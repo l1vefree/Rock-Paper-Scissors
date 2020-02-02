22bd147130d517697a2f71761760ed55f167325f
Ryan Bailey <baileyryan21@yahoo.com>
2020-02-01 21:39:36 -0500

    v2.

--- script.js
+++ script.js
@@ -2,8 +2,47 @@ let playerScore = 0;
 let computerScore = 0;
 
 function computerPlay() {
-	const selection = [ 'Rock', 'Paper', 'Scissors' ];
-	const randomSelection = Math.floor(Math.random() * 3);
-	let computerSelection = selection[randomSelection];
-	return computerSelection;
+	const computerPick = [ 'rock', 'paper', 'scissors' ];
+	const computerChoice = computerPick[Math.floor(Math.random() * 3)];
+	return computerChoice;
 }
+
+function playRound(playerSelection, computerSelection) {
+	if (playerSelection === computerSelection) {
+		alert('Draw!');
+	} else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+		playerScore++;
+		alert('You won the round! Rock beats scissors');
+	} else if (playerSelection === 'rock' && computerSelection === 'paper') {
+		computerScore++;
+		alert('You lost the round! Paper beats rock.');
+	} else if (playerSelection === 'paper' && computerSelection === 'rock') {
+		playerScore++;
+		alert('You won the round! Paper beats rock.');
+	} else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+		computerScore++;
+		alert('You lost the round! Scissors beats paper!');
+	} else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+		playerScore++;
+		alert('You won the round! Scissors beats paper.');
+	} else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+		computerScore++;
+		alert('You lost the round! Rock beats scissors');
+	}
+}
+
+function game() {
+	for (i = 0; i < 5; i++) {
+		let playerSelection = prompt('Pick Your Weapon: Rock, Paper or Scissors').toLowerCase();
+		let computerSelection = computerPlay();
+		playRound(playerSelection, computerSelection);
+		console.log(playerScore, computerScore);
+	}
+	if (playerScore > computerScore) {
+		alert("Congrats you've won the game!");
+	} else if (playerScore < computerScore) {
+		alert("Sorry you've lost the game!");
+	} else alert("You've tied");
+}
+
+game();
